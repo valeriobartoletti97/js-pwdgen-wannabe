5928c111623dbc643cdc91eb26931072808efc38
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-10-06 13:40:06 +0200

    created index.html and script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* chiedi all utente il suo nome
+chiedi cognome
+chiedi colore preferito
+scrivi sullapagina nomecognomecolorepreferito21 */
+
+const fixednumber = 21;
+
+let nome = prompt('Dimmi il tuo nome')
+console.log(nome)
+let cognome = prompt('Dimmiil tuo cognome')
+console.log(cognome)
+let colorePreferito = prompt('Qual è il tuo colore preferito')
+console.log(colorePreferito)
