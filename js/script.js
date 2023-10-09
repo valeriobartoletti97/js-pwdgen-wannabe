bc487ada55220919bff5ef59b3f42e1f52b3440a
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-10-09 08:29:54 +0200

    exercise finished

--- js/script.js
+++ js/script.js
@@ -5,9 +5,12 @@ scrivi sullapagina nomecognomecolorepreferito21 */
 
 const fixednumber = 21;
 
-let nome = prompt('Dimmi il tuo nome')
-console.log(nome)
-let cognome = prompt('Dimmiil tuo cognome')
-console.log(cognome)
-let colorePreferito = prompt('Qual è il tuo colore preferito')
+let nome = prompt('Dimmi il tuo nome');
+console.log(nome);
+let cognome = prompt('Dimmiil tuo cognome');
+console.log(cognome);
+let colorePreferito = prompt('Qual è il tuo colore preferito');
 console.log(colorePreferito)
+let password = nome + cognome + colorePreferito + fixednumber;
+console.log (password);
+document.getElementById("container-test").innerHTML = password;
